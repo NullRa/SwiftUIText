5a756e7c64208ea5daa0620d5949d1049a7f1a04
jyang <jTang@walkgame.com>
2021-10-07 16:34:17 +0800

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Apple on 2021/10/7.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
